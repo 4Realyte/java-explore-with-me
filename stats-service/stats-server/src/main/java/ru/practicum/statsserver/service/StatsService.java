b899ee3eeba88f6882c7176ebd7febf117c71ed7
IvanB <ibaletov@yandex.ru>
2023-07-14 08:04:58 +0300

    fix: minor fixes, prepare for client

--- stats-service/stats-server/src/main/java/ru/practicum/statsserver/service/StatsService.java
+++ stats-service/stats-server/src/main/java/ru/practicum/statsserver/service/StatsService.java
@@ -1,25 +1,13 @@
 package ru.practicum.statsserver.service;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import ru.practicum.statsserver.dao.StatsRepositoryImpl;
 import stats.EndpointHit;
 import stats.GetRequestStats;
 import stats.ViewStats;
 
 import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-public class StatsService {
+public interface StatsService {
+    void addHit(EndpointHit dto);
 
-    private final StatsRepositoryImpl repository;
-
-    public void addHit(EndpointHit dto) {
-        repository.addHit(dto);
-    }
-
-    public List<ViewStats> getAllStats(GetRequestStats request) {
-        return repository.getAllStats(request);
-    }
+    List<ViewStats> getAllStats(GetRequestStats request);
 }
